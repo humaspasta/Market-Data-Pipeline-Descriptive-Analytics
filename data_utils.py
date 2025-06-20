c3741f954d643c55c837f8691d19cd0703de527b
humaspasta <97994370+humaspasta@users.noreply.github.com>
2025-06-19 23:30:53 -0600

    added to data_utils and created project_1_data_pipeline
    Along with some exploratory data analysis

--- data_utils.py
+++ data_utils.py
@@ -3,7 +3,7 @@ import time
 import pandas as pd
 from joblib import Memory
 import os
-
+import numpy as np
 class Data:
 
     def __init__(self , tickers_path):
@@ -11,11 +11,10 @@ class Data:
         self.tickers_list = []
         self.ticker_data = []
         self.memory = Memory(os.path.join('.' , 'cache'))
-        self.get_ticker_data = self.memory.cache(self._get_ticker_data)
+        self.get_ticker_data_caching = self.memory1.cache(self._get_ticker_data)
         
 
     def get_tickers(self) -> None:
-    
         '''
         Sets the tickers we want to analyze. Tickers are accessed from symbols.txt. 
         '''
@@ -30,11 +29,32 @@ class Data:
     def _get_ticker_data(self , ticker:str) -> pd.DataFrame:
         return yf.download(ticker)
     
-    def get_ticker(self , ticker) -> None:
+    def get_ticker_data(self , ticker:str) -> None:
         '''
-        Appends all the ticker dataframe references to an array
+        Retrieves the DataFrame for a given ticker
         '''
-        return self.get_ticker_data(ticker)
+        return self.get_ticker_data_caching(ticker)
+    
+    def set_ticker_data(self , new_data):
+        pass
+
+    def log_data(self):
+        pass
+        
+
+    def clean_ticker_data(self , ticker):
+        data = self.get_ticker_data(ticker)
+        #Forward fill data and
+        new_data = data.resample('D').ffill()#will fill in missing days 
+
+        diff = new_data[new_data != data]
+        
+
+
+
+
+        
+
 
 
 
