e1a2bcf9567e17249100a3e0a677be4a65e653a1
humaspasta <97994370+humaspasta@users.noreply.github.com>
2025-06-23 17:17:41 -0600

    created parque file functions, fixed clean data method, semi fixed log_data method , added comments, added dividend calculation function

--- Test.py
+++ Test.py
@@ -1,10 +1,15 @@
+import os 
 from data_utils import Data
 
-data = Data('symbols.txt')
+
+data = Data(os.path.join('Data' , 'symbols.txt') , ('2024-06-16' , '2025-06-16') , os.path.join('Data' , 'logs.txt'))
+data.clear_cache()
 data.get_tickers()
-retireved_data = data.get_ticker_data(data.tickers_list[0])
-print(retireved_data.head())
 
-cleaned_data = data.clean_ticker_data(data.tickers_list[0])
+present_data = data.clean_ticker_data(data.tickers_list[0])
 
-print(data.get_ticker_data(data.tickers_list[0]).head())
+data.to_parquete('AAPL')
+data_parqed = data.get_parquet('AAPL')
+print(data_parqed)
+
+
--- data_utils.py
+++ data_utils.py
@@ -6,9 +6,22 @@ from joblib import Memory
 import os
 import numpy as np
 import fastparquet
+import datetime
 class Data:
 
     def __init__(self , tickers_path:str , date:tuple, log_path:str):
+        '''
+        tickers_path: The path to the tickers
+        date: a tuple that should be strictly of length 2 and contains values for the start and end date like so (start date , end date). 
+        Takes strings in %Y-%M-%D format. 
+        log_path: The path where logs should be noted
+
+        '''
+        if date == None:
+            raise ValueError('tuple is None')
+        elif len(date) > 2:
+            raise ValueError('Unexpected amount of dates')
+        
         self.tickers_path = tickers_path
         self.tickers_list = []
         self.memory = Memory(os.path.join('.' , 'Cache'))
@@ -33,6 +46,9 @@ class Data:
     
     #methods for retrieving and updating ticker data
     def _get_ticker_data(self , ticker:str) -> pd.DataFrame:
+        '''
+        inner function for retrieving ticker data
+        '''
         return yf.download(ticker , start=self.date[0] , end=self.date[1])
     
     def get_ticker_data(self , ticker:str) -> pd.DataFrame:
@@ -42,21 +58,30 @@ class Data:
         return self.get_ticker_data_caching(ticker)
     
     def update_ticker_data(self , ticker):
+        '''
+        updating ticker data
+        '''
         self.get_ticker_data_caching.clear(ticker)
         return self.get_ticker_data(ticker)
 
 
     #method for logging data
-    def log_data(self, information:pd.DataFrame) -> None:
+    def log_data(self, ticker:str , information:list) -> None:
         '''
-        Writes input dataframe to a txt file that stores the logs.
+        Writes added days to the log.txt file
         '''
+        
         with open(self.log_path , 'w') as tx:
-            tx.write('Logs')
-        information.to_csv(self.log_path, sep='\t', index=False)
+            tx.write('Logs for ' + ticker + '\n')
+            for date in information:
+                tx.write(str(date) + "\n")
+            tx.close()
                 
-    
+
     def clear_cache(self):
+        '''
+        clears the cache
+        '''
         for ticker in self.tickers_list:
             self.get_ticker_data_caching.clear(ticker)
             self.clean.clear(ticker)
@@ -66,27 +91,73 @@ class Data:
 
      #methods for cleaning data and storing back in cache   
     def clean_ticker_data(self , ticker):
-        return self.clean(ticker)
+        '''
+        cleans the data
+        '''
+        data = self.clean(ticker)
+        data.reset_index(inplace=True)
+        return data
 
-    def _clean_ticker_data(self , ticker):
+    def _clean_ticker_data(self , ticker:str):
+        '''
+        Inner function that manages the data cleaning
+        '''
         data = self.get_ticker_data(ticker)
         data.dropna(axis=0)
         #Forward fill data and
-        new_data = data.resample('D').ffill()#will fill in missing days 
+        new_data = data.resample('D').ffill()#will fill in missing days using fast forward fill
+        
+        new_dates = set(new_data.index.difference(data.index)) #finding data that was logged in the new_data
+        old_dates = set(data.index)
 
-        diff_data = data[data[not data.isin(new_data)]] #finding data that was logged in the new_data
-
-        self.log_data(diff_data) #logging changed data in new_data
+        date_diff = new_dates - old_dates #getting dates that are not common between the two
+        self.log_data(ticker , date_diff) # logging the dates that were added
+        
         return new_data
+        
 
     #converting data to parquete file
     def to_parquete(self , ticker:str) -> None:
-        data = self.get_ticker_data(ticker)
-        fastparquet.write(os.path.join('Data' , 'Clean' , f'{ticker}' + '.parq' ) , data)
+        '''
+        converts the data for a given ticker to a parquet file
+        '''
+        data = self.clean_ticker_data(ticker)
+        data_path = os.path.join('Data' , 'Clean' , f'{ticker}' + '.parq' )
+        if os.path.exists(data_path):
+            os.remove(data_path)
+        
+        data.to_parquet(data_path, engine='fastparquet')
 
+
+    def get_parquet(self , ticker):
+        '''
+        Retrieves the data of a given ticker from the respective parque file
+        '''
+        path = os.path.join('Data' , 'Clean' , f'{ticker}' + '.parq' )
+        return pd.read_parquet(path , engine='fastparquet')
     
 
+    #calculations
+    def calculate_dividend_error(self , ticker , date:str):
+        '''
+        Calculates the dividend error with respect to the average of the period analyzed.
+        '''
+        date_format = '%y-%m-%d'
+        
+        dt_object = datetime.strptime(date , date_format)
 
+        data = self.get_parquet(ticker)
+
+        close_values = data[('Close' , ticker)]
+        close_avg = close_values.sum()
+        
+
+        close_on_day = data[data[('Date' , '') == dt_object]][('Close' , ticker)]
+
+        error = abs(close_on_day - close_avg) / close_avg * 100
+
+    
+    
 
 
 
